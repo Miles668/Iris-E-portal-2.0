d1513358a49a828cbcd99dd1c8b663fc4c36c3ca
Miles668 <mushipejonathanmiles@gmail.com>
2026-06-13 05:54:45 +0200

    Update supabase-client.js

--- public/js/supabase-client.js
+++ public/js/supabase-client.js
@@ -1,8 +1,8 @@
 // supabase-client.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://YOUR-SUPABASE_URL.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_PUBLIC_ANON_KEY';
+const SUPABASE_URL = 'https://tqltylavwrsemhgdnorr.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxbHR5bGF2d3JzZW1oZ2Rub3JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExOTIxNDksImV4cCI6MjA5Njc2ODE0OX0.QyCfEf7Pif3-ZfhhwJ8PWUemi37orIZpiX2ZzpCRAyg';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
